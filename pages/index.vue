81e3968e11272858bae63f548ba1c34dca5165e8
Bearies <bearies@byzantine.network>
2018-05-31 15:04:46 -0400

    add page content

---
+++ pages/index.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="moon-monsters">
+    <div class="banner">
+      <div class="page-logo">
+        <img class="bk-image" src="~/assets/bk_placeholder.png" alt="backgroung image">
+        <img class="logo-text" src="~/assets/logo_moonmonsters.png" alt="Moon Monsters">
+      </div>
+    </div>
+    <div class="desc">
+      <p class="paragraph">Imagine waking up, on a moon in the middle of spacetime you're surrounded by monsters, and have no memory of exactly how you got here.</p>
+      <p class="paragraph">Uncover the mystery and create your own story as a Moon Monster, an intelligent extra-planetary race. As you seek allies and family you build your skills and defenses to stand your ground and explore the new territories you find.</p>
+      <p class="paragraph">Each week, venture out into the unknown to discover new secrets, obtain hidden treasures, and defeat a new monster, allowing you to unlock unique items, experiences, and earn MINED tokens.</p>
+    </div>
+    <div class="video">
+      <iframe width="680" height="400" src="https://www.youtube.com/embed/6Gf_kRE4MJU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+    <div class="white-paper">
+      <button class="whitepaper-btn">READ THE WHITEPAPER</button>
+    </div>
+    <div class="collaboration">
+      <div class="title">in collaboration with</div>
+      <div class="logos-row">
+        <div class="logos-cell"><img src="~/assets/logo_dispatchlabs.png" alt="Dispatch Labs"></div>
+        <div class="logos-cell"><img src="~/assets/logo_playchemy.png" alt="Playchemy"></div>
+      </div>
+    </div>
+    <div class="subscription">
+      <div class="title">Sign up to get notified when the game is released for IOS and Android.</div>
+      <div class="item"><input type="text" placeholder="enter emaill"></div>
+      <div class="item"><button>GET UPDATES</button></div>
+    </div>
+    <footer class="footer">
+      <div class="name">Moon Kid Studios</div>
+      <div class="email">hello@moonkidstudios.com</div>
+    </footer>
+  </div>  
+</template>
+
+<style scoped>
+  .moon-monsters {
+    background: #b3feff;
+    color: #212121;
+  }
+
+  .banner {
+    text-align: center;
+    height: 100vh;
+    width: 100vw;
+  }
+
+  .page-logo {
+    position: relative;
+    display: inline-block;
+    height: 100%;
+  }
+  .bk-image {
+    height: 100%;
+  }
+  .logo-text {
+    position: absolute;
+    top: 30%;
+    left: 10%;
+    width: 80%;
+  }
+
+  .desc {
+    margin: 0 auto;
+    padding: 60px 0;
+    width: 90%;
+    max-width: 780px;
+    text-align: center;
+    font-family: 'Minecraft', sans-serif;
+  }
+  .paragraph {
+    padding:  15px 0;
+    line-height: 2em;
+    letter-spacing: .1em;
+    color: #424242;
+  }
+  .paragraph:first-child {
+    margin: 0 auto;
+    max-width: 520px;
+  }
+
+  .video {
+    text-align: center;
+  }
+  
+  .white-paper {
+    padding: 80px 0;
+    text-align: center;
+  }
+  .whitepaper-btn {
+    padding: 1.2em 1.2em .8em;
+    background: #fff;
+    border: 1px solid #424242;
+    font-family: 'Minecraft', sans-serif;
+    font-size: 1em;
+    letter-spacing: .1em;
+    /* font-family: 'Pix-Riddim', sans-serif; */
+  }
+
+  .collaboration {
+    padding: 30px 0 40px;
+    text-align: center;
+  }
+  .collaboration .title {
+    font-family: 'Minecraft', sans-serif;
+    font-size: 1.1em;
+    letter-spacing: .1em;
+  }
+  .logos-row {
+    display: flex;
+    margin: 10px auto 0;
+    width: 100%;
+    max-width: 500px;
+  }
+  .logos-cell {
+    padding: 20px;
+  }
+  .logos-cell img {
+    width: 100%;
+  }
+
+  .subscription {
+    padding: 80px 24px;
+    background: #fff;
+  }
+  .subscription .title {
+    margin: 0 auto;
+    width: 100%;
+    max-width: 360px;
+    font-family: 'Minecraft', sans-serif;
+    letter-spacing: .1em;
+    text-align: center;
+    line-height: 1.8em;
+  }
+  .subscription .item {
+    text-align: center;
+  }
+  .subscription .item input {
+    margin-top: 30px;
+    padding: 20px;
+    height: 50px;
+    width: 90%;
+    max-width: 400px;
+    border: 1px solid #424242;
+    font-family: 'Minecraft', sans-serif;
+    font-size: .8em;
+    letter-spacing: .1em;
+    text-align: center;
+  }
+  .subscription .item button {
+    margin-top: 14px;
+    padding-top: .2em;
+    height: 50px;
+    width: 90%;
+    max-width: 300px;
+    background: #4452e8;
+    font-family: 'Minecraft', sans-serif;
+    font-size: .8em;
+    letter-spacing: .2em;
+    text-align: center;
+    color: #fff;
+  }
+
+  .footer {
+    padding: 20px;
+    background: #fff;
+    text-align: center;
+    font-size: .9em;
+    letter-spacing: .1em;
+  }
+  .footer .name,
+  .footer .email {
+    font-family: sans-serif;
+    font-weight: 600;
+    color: #424242;
+  }
+  .footer .email {
+    margin-top: 6px;
+    color: #4452e8;
+  }
+
+  @media (max-width: 767px) {
+    .moon-monsters {
+      padding-top: 10vh;
+      padding-bottom: 10vh;
+    }
+    .banner {
+      height: auto;
+    }
+    .bk-image {
+      width: 100%;
+    }
+  }
+
+/*   @media (min-width: 1440px) {
+    .bk-image {
+      max-width: 880px;
+    }
+    .page-logo img {
+      max-width: 880px;
+    }
+  } */
+    
+/* 
+  @media (max-width: 1023px) {
+    .page-logo {
+      width: 80%;
+    }
+  }
+
+  
+
+  @media (max-width: 425px) {
+    .page-logo img {
+      top: 50%;
+    }
+  } */
+</style>
