6e866c7354706bb9dd1ddb091773f80f78b4942d
Bearies <bearies@byzantine.network>
2018-05-31 15:42:03 -0400

    add layouts folder

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <nuxt/>
+</template>
+
+<style>
+  body, 
+  html {
+    margin: 0;
+    padding: 0;
+  }
+
+  * {
+    margin: 0;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    outline: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  @font-face {
+    font-family: "Minecraft";
+    src: url("~/assets/fonts/Minecraft.ttf");
+  }
+  @font-face {
+    font-family: "Pix-Riddim";
+    src: url("~/assets/fonts/Pix-Riddim.ttf");
+  }
+</style>
