85ce4f857677516538248eaa2a4db75fcd7991bb
Sam Hutch <sam@liminal.is>
2018-06-03 17:48:55 -0400

    minor tweaks, added titles, removed big moon, changed typeface

--- pages/index.vue
+++ pages/index.vue
@@ -16,9 +16,11 @@
       <div class="text-row">
         <div class="monster"><img class="monster-eggs" src="~/assets/images/eggs.png" alt="eggs"></div>
         <div class="text">
-          <p>Humans have discovered a far outpost of alien life. Around the Planet Zeta there are in orbit dozens of moons. The moons are inhabited by what appear to us as animal-like life forms. A nest of abandoned eggs has been procured to begin a study. Join the ranks of pioneering Xeno Anthropologists searching for the truth. Are these beings truly intelligent? Can we befriend and teach them? What will be learn?</p>
+          <div class="feature-title">Moon Monsters: XLab</div>
+          <p>Humans have discovered a far outpost of alien life.</p>
+          <p>Around the Planet Zeta there are in orbit dozens of moons. The moons are inhabited by what appear to us as animal-like life forms. A nest of abandoned eggs has been procured to begin a study. Join the ranks of pioneering Xeno Anthropologists searching for the truth. Are these beings truly intelligent? Can we befriend and teach them? What will be learn?</p>
           <p>Raise a Moon Monster from egg to young adulthood and help record behavioral traits. Care for, Name, and build an environment you believe it will respond to. What do we have on our hands? What are the implications for the Human race?</p>
-          <button class="comingsoon-btn">COMING SOON!</button>
+          <!-- <button class="comingsoon-btn">COMING SOON!</button> -->
         </div>
       </div>
     </div>
@@ -26,17 +28,18 @@
     <div class="white-paper">
       <div class="text-row">
         <div class="text">
+          <div class="feature-title">Moon Monsters: Mobile</div>
           <p>Imagine waking up, on a moon in the middle of spacetime. You’re surrounded by monsters, and have no memory of exactly how you got there.</p>
           <p>Uncover the mystery and create your own story as a Moon Monster, an intelligent extra-planetary race. As you seek allies and family you build your skills and defenses to stand your ground and explore the new territories you find.</p>
           <p>Each week, venture out into the unknown to discover new secrets, obtain hidden treasures, and defeat a new monster, allowing you to unlock unique items, experiences, and earn MINED tokens.</p>
-          <button class="whitepaper-btn">READ THE WHITEPAPER</button>
+          <!-- <button class="whitepaper-btn">READ THE WHITEPAPER</button> -->
         </div>
         <div class="monster"><img class="monster-tail" src="~/assets/images/feature2_monster.png" alt="monster"></div>
       </div>
     </div>
 
     <div id="mc_embed_signup" class="subscription">
-      <div class="title">Sign up to get notified when the game is released for IOS and Android.</div>
+      <div class="title">Sign up to get notified about new game releases and updates!</div>
       <form action="https://moonkidstudios.us18.list-manage.com/subscribe/post?u=863bebef61765efb23c38349f&amp;id=2b435c7848" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <div id="mc_embed_signup_scroll" class="align-center">
           <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
@@ -44,7 +47,7 @@
             <input type="text" name="b_863bebef61765efb23c38349f_9f54197e93" tabindex="-1" value="">
           </div>
           <div class="clear align-center">
-            <input type="submit" value="GET UPDATES" name="subscribe" id="mc-embedded-subscribe" class="button">
+            <input type="submit" value="SIGN ME UP" name="subscribe" id="mc-embedded-subscribe" class="button">
           </div>
         </div>
       </form>
@@ -52,16 +55,22 @@
 
     <footer class="footer">
       <div class="email">hello@moonkidstudios.com</div>
-      <div class="name">Moon Kid Studios</div>
-    </footer> 
+      <div class="name">Moon Kid Studios © 2018</div>
+    </footer>©
 
-  </div>  
+  </div>
 </template>
 
 <style scoped>
+
+
   .moon-monsters {
-    background: linear-gradient(90deg, #101b27, #434e5d);
+    /* background: linear-gradient(90deg, #101b27, #434e5d); */
+    background-color: #101b27;
   }
+
+  @import url('https://fonts.googleapis.com/css?family=PT+Mono');
+  font-family: 'PT Mono', monospace;
 
   .banner {
     text-align: center;
@@ -82,18 +91,23 @@
   }
   .desc {
     padding: 12% 0;
-    margin-top: -24%;
-    background-image: url('~/assets/images/largemoon.png');
+    margin-top: -20%;
+    /* background-image: url('~/assets/images/largemoon.png'); */
     background-size: 100%;
     background-position: top center;
     background-repeat: no-repeat;
     color: #fff;
     position: relative;
     z-index: 10;
-    font-family: 'Minecraft', sans-serif;
-    font-size: 2.4vw;
+    font-family: 'PT Mono', monospace;
+    font-size: 1.7vw;
+    text-align: center;
     text-shadow: .1em .1em black;
     letter-spacing: .1em;
+  }
+  .feature-title{
+    font-size: 2vw;
+    margin-bottom: 15px;
   }
   .paragraph {
     margin: 0 auto;
@@ -124,7 +138,8 @@
   }
   .text-row .text {
     position: relative;
-    font-family: 'Minecraft', sans-serif;
+    /* font-family: 'Minecraft', sans-serif; */
+    font-family: 'PT Mono', monospace;
     font-weight: 400;
     color: #fff;
     line-height: 1.6em;
@@ -139,13 +154,13 @@
     padding-left: 40px;
   }
 
-  .text-row .text p {  
+  .text-row .text p {
     padding:  10px 0;
-  }  
+  }
   .coming-soon .monster,
   .white-paper .monster {
     position: relative;
-  } 
+  }
 
   .monster-eggs {
     position: absolute;
@@ -189,7 +204,8 @@
     margin: 0 auto;
     width: 100%;
     max-width: 360px;
-    font-family: 'Minecraft', sans-serif;
+    /* font-family: 'Minecraft', sans-serif; */
+    font-family: 'PT Mono', monospace;
     letter-spacing: .1em;
     text-align: center;
     line-height: 1.8em;
@@ -205,7 +221,8 @@
     width: 90%;
     max-width: 400px;
     border: 1px solid #424242;
-    font-family: 'Minecraft', sans-serif;
+    /* font-family: 'Minecraft', sans-serif; */
+    font-family: 'PT Mono', monospace;
     font-size: .8em;
     letter-spacing: .1em;
     text-align: center;
@@ -218,7 +235,8 @@
     max-width: 300px;
     background: #fe5b96;
     border: 1px solid #fe5b96;
-    font-family: 'Minecraft', sans-serif;
+    /* font-family: 'Minecraft', sans-serif; */
+    font-family: 'PT Mono', monospace;
     font-size: .8em;
     letter-spacing: .2em;
     text-align: center;
@@ -231,7 +249,8 @@
 
   .footer {
     padding: 20px;
-    font-family: 'Minecraft', sans-serif;
+    /* font-family: 'Minecraft', sans-serif; */
+    font-family: 'PT Mono', monospace;
     font-size: .9em;
     letter-spacing: .1em;
     text-align: center;
@@ -252,6 +271,10 @@
   @media (max-width: 767px) {
     .desc {
       padding: 15% 0 4%;
+    }
+    .feature-title{
+      font-size: 6vw;
+      margin-top: 20px;
     }
     .paragraph {
       width: 90%;
